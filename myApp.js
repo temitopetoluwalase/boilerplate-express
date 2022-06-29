6db5642f76ee152df5dc9211f5afb63c348da7bd
Tolu <68949631+temitopetoluwalase@users.noreply.github.com>
2022-06-29 08:56:30 +0100

    serve form - get request

--- myApp.js
+++ myApp.js
@@ -1,7 +1,8 @@
 let express = require('express');
 let app = express();
+const absolutePath = __dirname + '/views/index.html';
 app.get('/', (req, res) => {
-  res.send('Hello Express');
+  res.sendFile(absolutePath);
 });
 
 module.exports = app;
