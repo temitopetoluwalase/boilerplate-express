0663cd06945330f2a35e5ebcd868ab5e8029d579
Tolu <68949631+temitopetoluwalase@users.noreply.github.com>
2022-07-15 19:09:21 +0100

    serve static assets

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 const absolutePath = __dirname + '/views/index.html';
+app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.sendFile(absolutePath);
 });
