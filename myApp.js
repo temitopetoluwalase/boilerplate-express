9000f34e44668c48cb90fcfb0057aa30f3e559e4
Tolu <68949631+temitopetoluwalase@users.noreply.github.com>
2022-07-15 19:42:30 +0100

    add env

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 const absolutePath = __dirname + '/views/index.html';
@@ -6,7 +7,12 @@ app.get('/', (req, res) => {
   res.sendFile(absolutePath);
 });
 app.get('/json', (req, res) => {
-  res.json({ message: "Hello json" });
+  res.json({
+    message:
+      process.env.MESSAGE_STYLE === 'uppercase'
+        ? 'Hello json'.toUpperCase()
+        : 'Hello json',
+  });
 });
 
 module.exports = app;
